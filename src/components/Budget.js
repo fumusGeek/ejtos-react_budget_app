ae97b50b35e21466277610416e74452ede74026d
fumusGeek <hanifantoni2@gmail.com>
2024-05-22 04:31:31 -0400

    finished task from ibm

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,18 +1,34 @@
-
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, dispatch } = useContext(AppContext);
+    const { budget, dispatch, currency } = useContext(AppContext);
     const handleBudgetChange = (event) => {
         const newBudget = Number(event.target.value);
-        dispatch({type:'SET_BUDGET', payload: newBudget})
+        dispatch({ type: 'SET_BUDGET', payload: newBudget })
+        if (newBudget >= 20000) {
+            alert('The value cannot exceed than 20000');
+        }
+    }
+    const handleCurrencyChange = (event) => {
+        const newCurrency = event.target.value;
+            dispatch({type: 'CHG_CURRENCY', payload: newCurrency})
     }
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £ </span>
-<input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
-</div>
+        <div>
+            <div className='alert alert-secondary'>
+                <span>Budget: {currency} </span>
+                <input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
+            </div>
+            <div>
+                <select className="custom-select" id="currency" onChange={handleCurrencyChange}>
+                    <option defaultValue value="$" name="dollar">$ Dollar</option>
+                    <option value="£" name="pound">£ Pound</option>
+                    <option value="€" name="euro">€ Euro</option>
+                    <option value="₹" name="ruppee">₹ Ruppe</option>
+                </select>
+            </div>
+        </div>
     );
 };
 export default Budget;
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,13 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
+    const { expenses, budget, currency } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
+    useEffect(() => {
+        if (budget < totalExpenses) {
+            alert(`Budget exceeded! You have spent £${totalExpenses - budget} more than your budget.`);
+        }
+    }, [budget, totalExpenses]);
     return (
         <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+            <span>Spent so far: {currency}{totalExpenses}</span>
         </div>
     );
 };
