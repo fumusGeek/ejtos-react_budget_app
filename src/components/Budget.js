26ca4bb47a4b6876f27865e8bb525646a97c48d3
fumusGeek <hanifantoni2@gmail.com>
2024-05-22 03:33:54 -0400

    changes on adding discpatch on budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,18 @@
 
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget, dispatch } = useContext(AppContext);
+    const handleBudgetChange = (event) => {
+        const newBudget = Number(event.target.value);
+        dispatch({type:'SET_BUDGET', payload: newBudget})
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £ </span>
+<input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
